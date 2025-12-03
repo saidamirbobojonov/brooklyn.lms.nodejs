57bea8971c10785fe2de23ea91339c3b3d774255
saidamirbobojonov <saidamirbobojonov@icloud.com>
2025-12-04 00:33:25 +0500

    Initial LMS backend commit

--- controllers/helloController.js
+++
@@ -1,6 +0,0 @@
-function helloController(req, res) {
-    res.writeHead(200, { 'Content-Type': 'application/json' });
-    res.end(JSON.stringify({ message: 'Добро пожаловать в LMS backend!' }));
-}
-
-module.exports = { helloController };
